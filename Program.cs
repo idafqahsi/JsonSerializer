63153c4680dd28bf59278c04ff7654479f8dae01
Fadi MI <32272687+idafqahsi@users.noreply.github.com>
2020-06-18 13:25:40 +0300

    reflection and json builder
    added field value into the list. Now we have field name, type, and value in the list. I also started with the json builder.

--- Program.cs
+++ Program.cs
@@ -1,68 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace ConsoleApp1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("-------------");
-            Configuration config = new Configuration(1, "fadi", new[] {
-    "123",
-    "456"
-   });
-            List<string> fields = NewMethod(config);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("-------------");
+			Configuration config = new Configuration(1, "fadi", new[] {
+				"123",
+				"456"
+			});
+			List<string> fields = NewMethod(config);
 
+			Console.ReadLine();
+		}
+		private static List<string> NewMethod(Object obj)
+		{
+			List<string> fieldNamesAndTypes = new List<string>();
 
+			Console.WriteLine("start");
+			Type type = obj.GetType();
+			FieldInfo[] fields = type.GetFields();
+			Console.WriteLine("fields and their types: ");
+			foreach (FieldInfo myField in fields)
+			{
+				// Console.WriteLine(myField.ToString());
 
+				string fieldName = myField.Name;
+				string fieldType = myField.FieldType.Name;
+				string fieldValue = "unknown";
 
-            Console.ReadLine();
-        }
-        private static List<string> NewMethod(Object obj)
-        {
-            List<string> fieldNamesAndTypes = new List<string>();
+				if (fieldType.Equals("String[]"))
+				{
+					// fieldValue = "xd";
+					//  var arr = myField.GetValue(obj);
 
-            Console.WriteLine("start");
-            Type type = obj.GetType();
-            FieldInfo[] fields = type.GetFields();
-            Console.WriteLine("fields and their types: ");
-            foreach (FieldInfo myField in fields)
-            {
-                // Console.WriteLine(myField.ToString());
+					String[] array = (string[])(myField.GetValue(obj) as Array);
 
-                string fieldName = myField.Name;
-                string fieldType = myField.FieldType.Name;
-                string fieldValue = "unknown";
+					foreach (string s in array)
+					{
+					//	Console.WriteLine(s);
+					}
 
-                if (fieldType == "String[]")
-                {
+					fieldValue = string.Join(",", array);
+					//fieldValue = String.Concat(array);
+					//fieldValue = "ah";
 
-                    //var x = myField.GetValue(obj);
-                    //string[] sRes = x.OfType<string>().ToArray();
-                    //                    foreach (var value in sRes)
-                    //                    {
-                    //                       Console.WriteLine(value);
-                    //                   }
-                }
-                else
-                {
-                    fieldType = myField.GetValue(obj).ToString();
-                }
+				}
+				else
+				{
+					fieldValue = myField.GetValue(obj).ToString();
+				}
 
-                fieldNamesAndTypes.Add(fieldName);
-                fieldNamesAndTypes.Add(fieldType);
-                fieldNamesAndTypes.Add(fieldValue);
-                //fieldNamesAndTypes.Add(myField.GetValue(obj));
+				fieldNamesAndTypes.Add(fieldName);
+				fieldNamesAndTypes.Add(fieldType);
+				fieldNamesAndTypes.Add(fieldValue);
 
-            }
+				// "{"Version":2,"DomainName":"www.training.com","IpAddresses":["192.168.1.8","192.168.1.2"]}"
 
-            for (int i = 0; i < fieldNamesAndTypes.Count; i++)
-            {
-                Console.WriteLine(fieldNamesAndTypes[i]);
-            }
-            return fieldNamesAndTypes;
-        }
-    }
+			}
+
+			for (int i = 0; i < fieldNamesAndTypes.Count; i++)
+			{
+				Console.WriteLine(fieldNamesAndTypes[i]);
+			}
+
+			var json = "";
+
+			for (int i = 0; i < fieldNamesAndTypes.Count / 2; i++)
+			{
+
+				json += "{\"" + fieldNamesAndTypes[i] + "\":" + fieldNamesAndTypes[i + 1] + "\",";
+
+			}
+
+			Console.WriteLine(json);
+
+			return fieldNamesAndTypes;
+		}
+	}
 }
